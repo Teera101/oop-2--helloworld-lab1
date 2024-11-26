ad1bec2c57af75a8565803b0f549388e0f56e915
Teera101 <119285336+Teera101@users.noreply.github.com>
2024-11-26 11:11:58 +0700

    add project

---
+++ hello world/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Teera Rewtawee";
+string student_id = "653450288-5";
+//string is Datatype
+//name is variable
+// "..." is value of variable
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("ich bin Teera Rewtawee");
+Console.WriteLine("My student id is 653450288-5");
